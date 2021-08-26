e285f9a9dbaaeb30f6ee6feb205212029055850a
alexy-merivot <68509172+alexy-merivot@users.noreply.github.com>
2021-08-26 03:55:13 +0200

    commit

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,28 +1,44 @@
 const fs = require("fs");
-const sauce = require("../models/sauce");
+const Sauce = require("../models/sauce");
 const user = require("../models/user");
 
 exports.newSauce = (req, res) => {
-  const sauceObject = JSON.parse(req.body.sauce);
+  let sauceObject = JSON.parse(req.body.sauce);
   delete sauceObject._id;
-  const sauce = new Sauce({
-      ...sauceObject,
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    });
+  let likeObject = {
+    likes : 0,
+    dislikes : 0,
+    usersLiked : [],
+    usersDisliked : [],
+  }
+  console.log("sauceObject avant Assign", sauceObject)
+  Object.assign(sauceObject, likeObject);
+  console.log("sauceObject après assign", sauceObject)
+  let sauce = new Sauce({
+    ...sauceObject,
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  });
+  sauce.likes = 0;
+  sauce.dislikes = 0;
+  console.log("sauce" ,sauce)
+  // console.log("sauceObject",  sauceObjet)
 
-    sauce.save()
-      .then(() => res.status(201).json({ message: 'Nouvel sauce enregistrée !'}))
-      .catch(error => res.status(400).json({ error }));
+  sauce.save()
+    .then(() => res.status(201).json({ message: 'Nouvel sauce enregistrée !'}))
+    .catch(error => res.status(400).json({ error }));
 }
 
 exports.getAllSauce = (req, res) => {
-  sauce.find()
-      .then(sauces => res.status(200).json(sauces))
-      .catch(error => res.status(400).json({ error }));
+  Sauce.find()
+    .then(sauces => res.status(200).json(sauces))
+    .catch(error => res.status(400).json({ error }));
 }
 
 exports.getASauce = (req, res) => {
-  sauce.findOne({ _id: req.params.id })
+  console.log(("toto1"))
+  console.log(req.body.sauce)
+  console.log("toto2")
+  Sauce.findOne({ _id: req.params.id })
     .then(sauce => res.status(200).json(sauce))
     .catch(error => res.status(404).json({ error }));
 }
@@ -32,19 +48,19 @@ exports.updateSauce  = (req, res) => {
     ...req.body,
     imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
   } : { ...req.body };
-  sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
     .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
     .catch(error => res.status(400).json({ error }));
 }
 
 exports.deleteSauce = (req, res) => {
-  sauce.findOne({_id: req.params.id})
+  Sauce.findOne({_id: req.params.id})
     .then(sauce => {
       const filename = sauce.imageUrl.split('/images/')[1];
       fs.unlink(`images/${filename}`, () => {
-        sauce.deleteOne({ _id: req.params.id })
+        Sauce.deleteOne({ _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
-        g.catch(error => res.status(400).json({ error }));
+        .catch(error => res.status(400).json({ error }));
       });
     })
     .catch(error => res.status(500).json({error}))
@@ -52,14 +68,17 @@ exports.deleteSauce = (req, res) => {
 
 exports.likeDislikeSauce = (req, res) => {
   console.log("debug like")
-  sauce.findOne({ _id: req.params.id})
+  Sauce.findOne({ _id: req.params.id})
   .then( sauce => {
+    console.log(sauce)
     if(req.body.like === 1){ // si le user like la sauce pour la première fois
       sauce.likes = sauce.likes++
       sauce.usersLiked.push(req.body.userId)
+      console.log( "sauce après like pour la première fois", sauce)
       sauce.updateOne({ _id: req.params.id }, { ...sauce, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Sauce likée !'}))
       .catch(error => res.status(400).json({ error })); // sauvegarder sauce sur mongoose
+      console.log( "sauce après like pour la première fois", sauce)
     }else if(req.body.like === -1){ // si le user dislike la sauce pour la première fois
       sauce.dislikes = sauce.dislikes++
       sauce.usersDisliked.push(user.userId)
@@ -69,15 +88,16 @@ exports.likeDislikeSauce = (req, res) => {
     }else if( req.body.like === 0){
       // création  de balise flag pour savoir si la sauce a déja été likée ou  dislikée
         var flag = 0;
-      for(var i=0; i<usersLiked.length; i++){
+      for(var i=0; i<sauce.usersLiked.length; i++){
         if( userId === usersLiked[i]) {flag = 1}
       }
-      for(var i=0; i<usersDisliked.length; i++){
+      for(var i=0; i<sauce.usersDisliked.length; i++){
         if( userId === usersDisliked[i]) {flag = 2;}
       }
       if(flag = 1) {  // si la sauce a déja été likée
         sauce.likes = sauce.likes--;
-        sauce.usersLiked.splice(userId)
+        console.log(req.body.userId)
+        sauce.usersLiked.splice(req.body.userId)
         sauce.updateOne({ _id: req.params.id }, { ...sauce, _id: req.params.id })
       .then(() => res.status(200).json({ message: 'Sauce délikée !'}))
       .catch(error => res.status(400).json({ error })); // sauvegarder sauce sur mongoose
@@ -89,6 +109,9 @@ exports.likeDislikeSauce = (req, res) => {
       .catch(error => res.status(400).json({ error })); // sauvegarder sauce sur mongoose
       }
     }
+    console.log("toto1")
+    console.log(sauce)
+    console.log("toto2")
   })
   .catch(error => {
     console.log(error)
@@ -96,59 +119,4 @@ exports.likeDislikeSauce = (req, res) => {
     })
 }
 
-
-
-// si user id existe dans tableau like ou dislike, alors mettre la  valeur de like a 0 et retirer l'user id du tableau
-
-// si user id n'existe  pas dans le tableau, alors si like = 1 ou -1 : ajouté +1 ou -1 a like de la sauce
-
-// usersLiked.find(userId => userId === req.body.userId)
-// userDisliked.find(userId => userId === req.body.userId)
-// if(userId){
-//   return Sauce.like === 0
-//   .then(() => res.status(200).json({ message: 'Sauce délikée !'}))
-//   .catch(error => res.status(400).json({ error }));
-
-// }else{
-
-// if( req.body.like === 0){
-//   var flag = 0;  // Initialement 0 - Introuvable
-// for(var i=0; i<usersLiked.length; i++) {
-//     if( userId === usersLiked[i]) {
-//          flag = 1;
-//     }
-// }
-// for(var i=0; i<usersDisliked.length; i++) {
-//   if( userId === usersDisliked[i]) {
-//        flag = 2;
-//   }
-// }
-// if(flag = 1){
-//   sauce.likes = sauce.likes--;
-//   usersLiked.splice(userId);
-// }else if(flag = 2){
-//   sauce.dislikes = sauce.dislikes--
-//   usersDisliked.splice(userId);
-// }
-// }
-
-
-
-// var flag = 0;  // Initialement 0 - Introuvable
-// for(var i=0; i<usersLiked.length; i++) {
-//     if( userId === usersLiked[i]) {
-//          flag = 1;
-//     }else{flag=3}
-// }
-// for(var i=0; i<usersDisliked.length; i++) {
-//   if( userId === usersDisliked[i]) {
-//        flag = 2;
-//   }else{flag=3}
-// }
-// if(flag = 1){
-//   sauce.likes = sauce.likes--;
-//   usersLiked.splice(userId);
-// }else if(flag = 2){
-//   sauce.dislikes = sauce.dislikes--
-//   usersDisliked.splice(userId);
-// }
+// likes et dislike de s'incrémente pas
